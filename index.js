34fb5122b3692bb6a82dc28d8bd88287664d1b05
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-12-27 14:22:41 +0900

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var mandelbrot = require('./native');
 
+var pixel_size = 0.01;
+var x0 = -2.0;
+var y0 = -1.0;
+
 function run() {
-  var context = document.getElementById('screen').getContext('2d');
-  var imageData = context.createImageData(300, 200);
+  var canvas = document.getElementById('screen');
+  var context = canvas.getContext('2d');
+  var width = canvas.width;
+  var height = canvas.height;
+
+  var imageData = context.createImageData(width, height);
   var buffer = new Buffer(imageData.data.buffer);
-  mandelbrot.mandelbrot(buffer, 300, 200, 0.01, -2.0, -1.0);
+  mandelbrot.mandelbrot(buffer, width, height, pixel_size, x0, y0);
+
   context.putImageData(imageData, 0, 0);
 }
