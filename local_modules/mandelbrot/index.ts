db3686e652dfe1c2674c1fee954bee559166b12b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-12-29 17:17:50 +0900

    fix type error + add JQuery and VSCode tasks

--- local_modules/mandelbrot/index.ts
+++ local_modules/mandelbrot/index.ts
@@ -2,7 +2,7 @@ module Mandelbrot {
   const addon = require('./native');
 
   export function mandelbrot(image: ImageData, pixel_size: number, x0: number, y0: number): void {
-    let buffer = image.data.buffer;
+    let buffer = new Buffer(image.data.buffer);
     addon.mandelbrot(buffer, image.width, image.height, pixel_size, x0, y0);
   }
 }
