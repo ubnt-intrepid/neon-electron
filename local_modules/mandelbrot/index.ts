5d365f78394971038b5af489b2e8dbdcf3da30f0
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-12-31 23:07:01 +0900

    refactor

--- local_modules/mandelbrot/index.ts
+++ local_modules/mandelbrot/index.ts
@@ -4,38 +4,39 @@ export module Native {
   export function mandelbrot(canvas: HTMLCanvasElement, pixel_size: number, x0: number, y0: number): void {
     console.log("[debug] call Native.mandelbrot()");
 
-    var buffer = new Buffer(canvas.width * canvas.height * 4);
-    addon.mandelbrot(buffer, canvas.width, canvas.height, pixel_size, x0, y0);
+    var context = canvas.getContext('2d');
 
-    var image = new ImageData(canvas.width, canvas.height);
-    image.data.set(new Uint8ClampedArray(buffer));
+    var image = context.getImageData(0, 0, canvas.width, canvas.height);
 
-    canvas.getContext('2d').putImageData(image, 0, 0);
+    addon.mandelbrot(Buffer.from(image.data.buffer), canvas.width, canvas.height, pixel_size, x0, y0);
+
+    context.putImageData(image, 0, 0);
   }
 }
 
 export module Wasm {
   export const Module = require('./wasm');
 
+  const _mandelbrot = Module.cwrap('mandelbrot', undefined,
+      ['number', 'number', 'number', 'number', 'number', 'number', 'number']);
+
   export function mandelbrot(canvas: HTMLCanvasElement, pixel_size: number, x0: number, y0: number): void {
     console.log("[debug] call Wasm.mandelbrot()");
 
-    // Allocate memory on Emscripten heap, and get pointer.
+    var context = canvas.getContext('2d');
+    var image = context.getImageData(0, 0, canvas.width, canvas.height);
+
     let n_bytes = canvas.width * canvas.height * 4;
-    let ptr = Module._malloc(n_bytes);
-    // Copy data to Emscripten heap (directry accessed from Module.HEAPU8)
-    var data_heap = new Uint8Array(Module.HEAPU8.buffer, ptr, n_bytes);
+    // Create a heat to interact with Emscripten's function
+    let ptr:number = Module._malloc(n_bytes);
+
     // Call function
-    Module.ccall('mandelbrot', undefined,
-                ['number', 'number', 'number', 'number', 'number', 'number', 'number'],
-                [data_heap.byteOffset, data_heap.length, canvas.width, canvas.height, pixel_size, x0, y0]);
+    _mandelbrot(ptr, n_bytes, canvas.width, canvas.height, pixel_size, x0, y0);
 
-    // create the instance of ImageData.
-    var image = new ImageData(canvas.width, canvas.height);
-    image.data.set(new Uint8ClampedArray(data_heap.buffer, data_heap.byteOffset, data_heap.length));
+    image.data.set(new Uint8ClampedArray(Module.HEAPU8.buffer, ptr, n_bytes));
+    context.putImageData(image, 0, 0);
+
     // Free memory.
-    Module._free(data_heap.byteOffset);
-
-    canvas.getContext('2d').putImageData(image, 0, 0);
+    Module._free(ptr);
   }
 }
