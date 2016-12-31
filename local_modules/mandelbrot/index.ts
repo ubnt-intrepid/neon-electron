5aac13b65429c00b9cd1ede526db79464156a98c
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-01-01 02:18:51 +0900

    refactor

--- local_modules/mandelbrot/index.ts
+++ local_modules/mandelbrot/index.ts
@@ -3,10 +3,12 @@ module Native {
   const Raw = require('./native');
 
   export class Backend {
-    call(canvas:any, context:any, image:any, pixel_size: number, x0: number, y0: number) {
-      let buffer = Buffer.from(image.data.buffer);
-      Raw.mandelbrot(buffer, canvas.width, canvas.height, pixel_size, x0, y0);
-      context.putImageData(image, 0, 0);
+    call(image: ImageData, width: number, height: number, pixel_size: number, x0: number, y0: number) {
+      Raw.mandelbrot(Buffer.from(image.data.buffer), width, height, pixel_size, x0, y0);
+    }
+
+    getType(): BackendType {
+      return BackendType.Native;
     }
 
     dispose() {}
@@ -22,30 +24,36 @@ module AsmJs {
               undefined,
               ['number', 'number', 'number', 'number', 'number', 'number', 'number']);
 
-  function allocateArray(n_bytes: number): Uint8ClampedArray {
-    let ptr = Raw._malloc(n_bytes);
-    return new Uint8ClampedArray(Raw.HEAPU8.buffer, ptr, n_bytes);
-  }
-
   export class Backend {
-    call(canvas:any, context:any, image:any, pixel_size: number, x0: number, y0: number) {
+    buffer: Uint8ClampedArray;
+
+    constructor(n_bytes: number) {
       // Create a heap to interact with Emscripten's function
-      let buffer = allocateArray(image.data.buffer.byteLength);
-
-      // Call function
-      _mandelbrot(buffer.byteOffset, buffer.length, canvas.width, canvas.height, pixel_size, x0, y0);
-
-      image.data.set(buffer);
-      context.putImageData(image, 0, 0);
-
-      // Free memory.
-      Raw._free(buffer.byteOffset);
+      let ptr = Raw._malloc(n_bytes);
+      this.buffer = new Uint8ClampedArray(Raw.HEAPU8.buffer, ptr, n_bytes);
     }
 
-    dispose() {}
+    call(image: ImageData, width: number, height: number, pixel_size: number, x0: number, y0: number) {
+      if (this.buffer === null) {
+        return;
+      }
+      _mandelbrot(this.buffer.byteOffset, this.buffer.length, width, height, pixel_size, x0, y0);
+      image.data.set(this.buffer);
+    }
+
+    getType(): BackendType {
+      return BackendType.AsmJs;
+    }
+
+    dispose() {
+      if (this.buffer !== null) {
+        // Free memory.
+        Raw._free(this.buffer.byteOffset);
+        this.buffer = null;
+      }
+    }
   }
 }
-
 
 export enum BackendType {
   Native, // use native module
@@ -56,38 +64,43 @@ type Backend = Native.Backend | AsmJs.Backend;
 
 
 export class MandelbrotRenderer {
-  readonly canvas: HTMLCanvasElement;
-  readonly context: CanvasRenderingContext2D;
-  readonly image: ImageData;
-  backend: [BackendType, Backend];
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
+  image: ImageData;
+  backend: Backend;
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
     this.image = this.context.getImageData(0, 0, canvas.width, canvas.height);
-    this.backend = [BackendType.Native, new Native.Backend()];
+    this.backend = new Native.Backend();
+
+    document.addEventListener('close', () => {
+      this.backend.dispose();
+    });
   }
 
   switchBackend(backendType: BackendType) {
-    if (backendType === this.backend[0]) {
+    if (backendType === this.backend.getType()) {
       return;
     }
 
-    this.backend[0] = backendType;
-    this.backend[1].dispose();
+    this.backend.dispose();
     switch (backendType) {
       case BackendType.Native:
         console.log("[debug] switch to Native.Backend");
-        this.backend[1] = new Native.Backend();
+        this.backend = new Native.Backend();
         break;
       case BackendType.AsmJs:
         console.log("[debug] switch to AsmJs.Backend");
-        this.backend[2] = new AsmJs.Backend();
+        let n_bytes = this.image.data.buffer.byteLength;
+        this.backend = new AsmJs.Backend(n_bytes);
         break;
     }
   }
 
   apply(pixel_size: number, x0: number, y0: number): void {
-    this.backend[1].call(this.canvas, this.context, this.image, pixel_size, x0, y0);
+    this.backend.call(this.image, this.canvas.width, this.canvas.height, pixel_size, x0, y0);
+    this.context.putImageData(this.image, 0, 0);
   }
 }
